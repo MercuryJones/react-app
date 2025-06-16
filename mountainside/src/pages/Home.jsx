04555a6438a54c37fbd547c77c402205bffa3cab
MercuryJones <johnarcherhatch@icloud.com>
2025-06-15 21:22:51 -0400

    testing issues

--- mountainside/src/pages/Home.jsx
+++ mountainside/src/pages/Home.jsx
@@ -1,16 +1,38 @@
+// import React from 'react';
+// import Banner from '../components/Banner';
+// import About from '../components/About';
+// import Gallery from '../components/Gallery';
+// import Restaurants from '../components/Restaurants';
+
+// const Home = () => {
+//   return (
+//     <>
+//       <Banner />
+//       <About />
+//       <Gallery />
+//       <Restaurants />
+//     </>
+//   );
+// };
+
+// export default Home;
+
+
 import React from 'react';
 import Banner from '../components/Banner';
-import About from '../components/About';
-import Gallery from '../components/Gallery';
-import Restaurants from '../components/Restaurants';
+// import About from '../components/About';
+// import Gallery from '../components/Gallery';
+// import Restaurants from '../components/Restaurants';
 
 const Home = () => {
   return (
     <>
+      <div style={{ padding: '2rem', background: 'white' }}>
+        <h1>Testing Render</h1>
+        <p>If you see this message, Home is rendering correctly.</p>
+      </div>
+
       <Banner />
-      <About />
-      <Gallery />
-      <Restaurants />
     </>
   );
 };
