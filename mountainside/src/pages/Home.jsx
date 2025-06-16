64f34356c49fe58350c904a100afcf2a8fc1c26e
MercuryJones <johnarcherhatch@icloud.com>
2025-06-15 20:53:52 -0400

    checking home page issues

--- mountainside/src/pages/Home.jsx
+++ mountainside/src/pages/Home.jsx
@@ -4,15 +4,24 @@ import About from '../components/About';
 import Gallery from '../components/Gallery';
 import Restaurants from '../components/Restaurants';
 
-const Home = () => {
-  return (
-    <>
-      <Banner />
-      <About />
-      <Gallery />
-      <Restaurants />
-    </>
-  );
-};
+// const Home = () => {
+//   return (
+//     <>
+//       <Banner />
+//       <About />
+//       <Gallery />
+//       <Restaurants />
+//     </>
+//   );
+// };
 
+const Home = () => {
+    return (
+      <div style={{ padding: '2rem' }}>
+        <h1>Hello from Home!</h1>
+        <p>If you see this, routing is working.</p>
+      </div>
+    );
+  };
+  
 export default Home;
