61f5158e3444cbb2aadd6f574d0f785ec5f8ff93
MercuryJones <johnarcherhatch@icloud.com>
2025-06-15 21:02:47 -0400

    fixing main routing

--- mountainside/src/pages/Home.jsx
+++ mountainside/src/pages/Home.jsx
@@ -4,24 +4,15 @@ import About from '../components/About';
 import Gallery from '../components/Gallery';
 import Restaurants from '../components/Restaurants';
 
-// const Home = () => {
-//   return (
-//     <>
-//       <Banner />
-//       <About />
-//       <Gallery />
-//       <Restaurants />
-//     </>
-//   );
-// };
-
 const Home = () => {
-    return (
-      <div style={{ padding: '2rem' }}>
-        <h1>Hello from Home!</h1>
-        <p>If you see this, routing is working.</p>
-      </div>
-    );
-  };
-  
+  return (
+    <>
+      <Banner />
+      <About />
+      <Gallery />
+      <Restaurants />
+    </>
+  );
+};
+
 export default Home;
