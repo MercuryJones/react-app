f878c67a8928cb5bc4c10278ae7303041f090fe9
MercuryJones <johnarcherhatch@icloud.com>
2025-06-15 22:58:49 -0400

    more main.jsx changes

--- mountainside/src/main.jsx
+++ mountainside/src/main.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom'; // ✅ import this
 import App from './App';
 import './App.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter basename={import.meta.env.BASE_URL}>  {/* ✅ wrap App in Router */}
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
